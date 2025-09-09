363cd1db9e1db23962be0e6327446618740df6f5
BhimPrasadAdhikari <bhimprasadadhikari98@gmail.com>
2025-09-09 21:43:29 +0530

    feat: Implement inorder tree traversal algorithm

--- binary_tree.py
+++ binary_tree.py
@@ -7,8 +7,19 @@ class Node:
 
 
 # Building a short perfect tree.
+# A perfect tree has 2^h leaf nodes and 2^(h + 1) -1 total nodes.
 root = Node(10)
 root.left = Node(20)
 root.right = Node(30)
 
-        
+# Traversing the tree
+
+# 1. Inorder traverse (left sub-tree -> root -> right sub-tree)
+def inorder(root_node):
+    if root_node is None:
+        return
+    inorder(root_node.left)
+    print(root_node.data)
+    inorder(root_node.right)
+    
+inorder(root)    
