71affae8c57d3f7b5da21d4f352ef6d29b5675a7
BhimPrasadAdhikari <bhimprasadadhikari98@gmail.com>
2025-09-09 21:51:08 +0530

    feat(BT): Implement preorder and postorder traversal

--- binary_tree.py
+++ binary_tree.py
@@ -16,10 +16,31 @@ root.right = Node(30)
 
 # 1. Inorder traverse (left sub-tree -> root -> right sub-tree)
 def inorder(root_node):
+    """Traverse the tree in left sub-tree -> root -> right sub-tree"""
     if root_node is None:
         return
     inorder(root_node.left)
     print(root_node.data)
     inorder(root_node.right)
     
+def preorder(root_node):
+    """Traverse the tree in root -> left sub-tree -> right sub-tree flow"""
+    if root_node is None:
+        return
+    print(root_node.data)
+    preorder(root_node.left)
+    preorder(root_node.right)
+    
+def postorder(root_node):
+    """Traverse the tree in left sub-tree -> right sub-tree -> root flow"""
+    if root_node is None:
+        return
+    preorder(root_node.left)
+    preorder(root_node.right)
+    print(root_node.data)
+    
+    
+
 inorder(root)    
+preorder(root)
+postorder(root)
