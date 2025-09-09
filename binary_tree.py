b8e7cd7be569ff2389aaf3fcc136d01692ba48af
BhimPrasadAdhikari <bhimprasadadhikari98@gmail.com>
2025-09-09 21:32:54 +0530

    feat: Build a short perfect tree

--- binary_tree.py
+++ binary_tree.py
@@ -4,5 +4,11 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
-        
+
+
+# Building a short perfect tree.
+root = Node(10)
+root.left = Node(20)
+root.right = Node(30)
+
         
