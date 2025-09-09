6788e346eb78d61bdc05432e4946dce08013dca2
BhimPrasadAdhikari <bhimprasadadhikari98@gmail.com>
2025-09-09 21:25:48 +0530

    feat: Create initial Node class structure

---
+++ binary_tree.py
@@ -0,0 +1,8 @@
+class Node:
+    """A single node in a binary tree"""
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+        
